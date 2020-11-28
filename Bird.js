3e16f70ba3bccab586bee6ee4fd37a74621cf71b
AvadaKedavra95 <mcefunds@gmail.com>
2020-11-28 09:56:05 +0530

    SlingShot added

--- Bird.js
+++ Bird.js
@@ -1,12 +1,21 @@
 class Bird extends BaseClass {
   constructor(x,y){
+
+    
     super(x,y,50,50);
+    var option = {
+      density : 50,
+      friction : 0.01,
+      restitution : 0.0001
+    }
+    this.body=Bodies.rectangle(x,y,50,50,option)
     this.image = loadImage("sprites/bird.png");
+    World.add(world,this.body)
   }
 
   display() {
-    this.body.position.x = mouseX;
-    this.body.position.y = mouseY;
+    // this.body.position.x = mouseX;
+    // this.body.position.y = mouseY;
     super.display();
   }
 }
